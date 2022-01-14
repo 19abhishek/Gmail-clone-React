31ef9bf7d6d334508fc51085d3836590e4858837
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-14 14:05:13 +0530

    Sidebar completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,18 @@
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import React from "react";
 import "./Sidebar.css";
 import AddIcon from "@material-ui/icons/Add";
+import SidebarOption from "./SidebarOption";
+import InboxIcon from "@material-ui/icons/Inbox";
+import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 function Sidebar() {
   return (
@@ -12,6 +23,31 @@ function Sidebar() {
       >
         Compose
       </Button>
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={66}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={12} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={6} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={3} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" number={10} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={5} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={4} />
+      <div class="sidebar--footer">
+        <div class="sidebar--footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
