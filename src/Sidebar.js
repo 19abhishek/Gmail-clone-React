2f3b389dbd915c88d123af82e7e600ffd0cef632
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-14 11:16:32 +0530

    Header section completed

---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+import AddIcon from "@material-ui/icons/Add";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <Button
+        startIcon={<AddIcon fontSize="large" />}
+        className="sidebar--compose"
+      >
+        Compose
+      </Button>
+    </div>
+  );
+}
+
+export default Sidebar;
