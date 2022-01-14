20fc5b660c9e10115a97d91485579c4ff9dd2255
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-15 01:39:29 +0530

    Reducer setup

---
+++ src/store/mailSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const mailSlice = createSlice({
+  name: "mail",
+  initialState: {
+    value: 0,
+  },
+  reducers: {},
+});
+
+export const selectMail = (state) => state.mail.value;
+
+export default mailSlice.reducer;
